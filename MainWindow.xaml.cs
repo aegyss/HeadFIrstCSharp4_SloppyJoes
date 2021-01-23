513631733bddf575de5ffd39f9a0863cacfd7ab3
Arjay Orbita <planetorbita@gmail.com>
2021-01-23 10:52:03 -0800

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HeadFIrstCSharp4_SloppyJoes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MakeTheMenu();
+        }
+
+        public void MakeTheMenu()
+        {
+            MenuItem[] menuItems = new MenuItem[5];
+            for ( int i = 0; i < 5; i++)
+            {
+                if (i < 3)
+                {
+                    menuItems[i] = new MenuItem();
+                } 
+                else
+                {
+                    menuItems[i] = new MenuItem() { Breads = new string[] { "Plain Bagel", "Onion Bagel", "Pumpernickel Bagel", "Everything Bagel" } };
+                }
+            }
+
+            item1.Text = menuItems[0].GenerateMenuItem
+        }
+    }
+}
---
+++ MenuItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFIrstCSharp4_SloppyJoes
+{
+    class MenuItem
+    {
+        public Random Randomizer = new Random();
+        public string[] Proteins = { "Roast Beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofu" };
+        public string[] Condiments = { "Yellow Mustard", "Brown Mustard", "Honey Mustard", "Mayo", "Relish", "French Dressing", };
+        public string[] Breads = { "Rye", "White", "Wheat", "Pumpernickel", "A Roll" };
+
+        public string GenerateMenuItem()
+        {
+            string RandomProtein = Proteins[Randomizer.Next(Proteins.Length)];
+            string RandomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string RandomBread = Breads[Randomizer.Next(Breads.Length)];
+            return $"{RandomProtein} with {RandomCondiment} on {RandomBread}.";
+        }
+
+        public string RandomPrice()
+        {
+            decimal bucks = Randomizer.Next(2, 5);
+            decimal cents = Randomizer.Next(1, 98);
+            decimal price = bucks + (cents * .01M);
+            return price.ToString("c");
+        }
+    }
+
+    
+}
